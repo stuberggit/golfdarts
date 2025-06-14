24eb3194bc352419c3522ac4a5bbeff4e43773e5
stuberggit <stuberg+github@gmail.com>
2025-06-14 12:51:11 -0500

    Update script.js

--- script.js
+++ script.js
@@ -197,7 +197,7 @@ function showScoreAnimation(message, color = "#0a3") {
   el.innerText = message;
   el.style.animation = "none";
   void el.offsetWidth;
-  el.style.animation = "popIn 0.6s ease-out";
+  el.style.animation = "popIn 0.8s ease-out";
   setTimeout(() => el.innerText = "", 1000);
 }
 
