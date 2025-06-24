0f17121d3bc1a0d83862dc0cde7cec6baef90444
stuberggit <stuberg+github@gmail.com>
2025-06-24 15:45:06 -0500

    Update script2.js

--- script2.js
+++ script2.js
@@ -164,7 +164,7 @@ function submitPlayerScore() {
   }
   const player = players[currentPlayerIndex];
 if (hits === 6) {
-  const isShanghai = confirm(`Was this a Shanghai (1x, 2x, and 3x of ${currentHole})? <br>Cancel to score -2 and return to game. <br>OK to accept humiliating defeat`);
+  const isShanghai = confirm(`Was this a Shanghai (1x, 2x, and 3x of ${currentHole})? Cancel to score -2 and return to game. OK to accept humiliating defeat`);
   if (isShanghai) {
     showShanghaiWin(player.name);
     return; // skip rest of the scoring logic
@@ -206,7 +206,7 @@ if (hits === 6) {
 
         const names = tied.map(p => `"${p.name}"`).join(" and ");
         document.getElementById("scoreInputs").innerHTML = `
-          <h2>${names} tie! On to <b>Sudden Death!</b></h2>
+          <h2>${names} tie! On to Sudden Death!</h2>
           <button onclick="showHole()" class="primary-button full-width">Continue</button>
 
         `;
@@ -448,7 +448,7 @@ function loadGameState() {
   const state = JSON.parse(saved);
   if (!state || !state.players || state.players.length === 0) return;
 
-  if (confirm("Want to keep playing? <br>OK to return to your game. <br>Cancel to restart")) {
+  if (confirm("Want to keep playing? OK to return to your game. Cancel to restart")) {
     players = state.players;
     currentHole = state.currentHole;
     currentPlayerIndex = state.currentPlayerIndex;
